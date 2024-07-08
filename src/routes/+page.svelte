e10b816a41ba697c462975962e339b71044a8e4e
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-07-08 20:07:32 +0530

    first template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,53 @@
-<h1>Welcome to Summer Internship 2024</h1>
+<header class="bg-white py-4 shadow-md">
+    <div class="container mx-auto px-4">
+        <h1 class="text-2xl font-bold font-semibold">
+            Jasmita
+        </h1>
+    </div>
+    
+</header>
+
+<div class="container mx-auto md:my-5">
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+<!-- Post -->
+ <div>
+    <img class="w-full" src="https://picsum.photos/600" alt="Post">
+    <div class= "text-sm py-2">
+<span class="font-bold">
+    User-name
+</span>
+<span class="text-gray-700">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed similique error minima dolore quis repellat ex, vero tempora, in, quia magni dolorum numquam soluta! Quos amet ex hic voluptatem doloribus.
+    Neque nemo pariatur, cupiditate provident veniam voluptatem, odio dolor aut esse iste tempore ex est laboriosam sed minus voluptate temporibus at sunt reprehenderit. Omnis quae temporibus totam quaerat! A, quis?
+</span>
+    </div>
+ </div>
+
+ <div>
+    <img class="w-full" src="https://picsum.photos/600" alt="Post">
+    <div class= "text-sm py-2">
+<span class="font-bold">
+    User-name
+</span>
+<span class="text-gray-700">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed similique error minima dolore quis repellat ex, vero tempora, in, quia magni dolorum numquam soluta! Quos amet ex hic voluptatem doloribus.
+    Neque nemo pariatur, cupiditate provident veniam voluptatem, odio dolor aut esse iste tempore ex est laboriosam sed minus voluptate temporibus at sunt reprehenderit. Omnis quae temporibus totam quaerat! A, quis?
+</span>
+    </div>
+ </div>
+
+ <div>
+    <img class="w-full" src="https://picsum.photos/600" alt="Post">
+    <div class= "text-sm py-2">
+<span class="font-bold">
+    User-name
+</span>
+<span class="text-gray-700">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed similique error minima dolore quis repellat ex, vero tempora, in, quia magni dolorum numquam soluta! Quos amet ex hic voluptatem doloribus.
+    Neque nemo pariatur, cupiditate provident veniam voluptatem, odio dolor aut esse iste tempore ex est laboriosam sed minus voluptate temporibus at sunt reprehenderit. Omnis quae temporibus totam quaerat! A, quis?
+</span>
+    </div>
+ </div>
+    </div>
+</div>
